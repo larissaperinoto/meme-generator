6b8fbc0be1236eb584de14c876dee37b18c5d98f
Larissa Perinoto <perinotolarissa@gmail.com>
2022-07-11 15:12:08 -0300

    [Larissa Perinoto] Project meme generator [Requisito 2]

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function showInput() {
+function showInputText() {
   const containerText = document.getElementById('meme-text');
   const inputText = document.getElementById('text-input');
-  inputText.addEventListener('keyup', function () {
+  inputText.addEventListener('keyup', () => {
     containerText.innerText = inputText.value;
-  })
+  });
 }
 
-showInput();
+showInputText();
+
+const file = document.getElementById('meme-insert');
+const photo = document.getElementById('meme-image');
+
+file.addEventListener('change', () => {
+  const reader = new FileReader();
+  reader.onload = () => {
+    photo.src = reader.result;
+  };
+
+  reader.readAsDataURL(file.files[0]);
+});
